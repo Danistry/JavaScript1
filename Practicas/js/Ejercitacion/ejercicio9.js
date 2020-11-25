5c6bea92545f401082fca68cded825a73331c1fb
Daniel ojeda <69994654+Danistry@users.noreply.github.com>
2020-11-25 19:07:47 -0300

    Add files via upload

--- Practicas/js/Ejercitacion/ejercicio9.js
+++ Practicas/js/Ejercitacion/ejercicio9.js
@@ -39,18 +39,50 @@ class Pelicula {
     this.calificacion = calificacion
 
     this.validarIMBD(id)
+    this.validarTitulo(titulo)
+    this.validarDirector(director)
+    this.validarEstreno(estreno)
+    this.validarPais(pais)
+    this.validarGeneros(generos)
+    this.validarCalificacion(calificacion)
   }
+
+  //Lista de generos estaticos, atributo estatico
+  static get listaGeneros() {
+    return ['Action', 'Adult', 'Adventure', 'Animation', 'Biography', 'Comedy', 'Crime', 'Documentary', 'Drama',
+      'Family', 'Fantasy', 'Film Noir', 'Game-Show', 'History', 'Horror', 'Musical', 'Music', 'Mystery', 'News', 'Reality-TV',
+      'Romance', 'Sci-Fi', 'Short', 'Sport', 'Talk-Show', 'Thriller', 'War', 'Western']
+  }
+
+  static generosAceptados() {
+    return console.info(`Los generos aceptados son: ${Pelicula.listaGeneros.join(', ')}`)
+  }
+  //Metodos
   validarCadena(propiedad, valor) {
     if (!valor) return console.log(`${propiedad} "${valor}" esta vacio`);
     if (typeof valor !== 'string') return console.error(`${propiedad} "${valor}" ingresado no es una cadena de texto`)
 
     return true
   }
-  validarLongitudCadena(propiedad, valor, longitud){
-    if(valor.length > longitud) return console.error(`${propiedad} "${valor}" excede el numero de caracteres permitidos (${longitud})`)
-  return true
-  }
+  validarNumero(propiedad, valor) {
+    if (!valor) return console.log(`${propiedad} "${valor}" esta vacio`);
+    if (typeof valor !== 'number') return console.error(`${propiedad} "${valor}" ingresado no es una cadena de texto`)
 
+    return true
+  }
+  validarArreglo(propiedad, valor) {
+    if (!valor) return console.log(`${propiedad} "${valor}" esta vacio`);
+    if (!(valor instanceof Array)) return console.log(`${propiedad} "${valor}" ingresado, NPO es un arreglo`);
+
+    for (let cadena of valor) {
+      if (typeof cadena !== 'string') return console.error(`El valor "${cadena} ingresado, NO es una cadena de texto"`)
+    }
+    return true
+  }
+  validarLongitudCadena(propiedad, valor, longitud) {
+    if (valor.length > longitud) return console.error(`${propiedad} "${valor}" excede el numero de caracteres permitidos (${longitud})`)
+    return true
+  }
   validarIMBD(id) {
     if (this.validarCadena('IMBD ID', id))
       if (!(/^([a-z]){2}([0-9]){7}$/.test(id)))
@@ -58,14 +90,56 @@ class Pelicula {
         debe tener 9 caracteres, los 2 primeros letras minusculas, las 7 restantes numeros`)
   }
 
-
   validarTitulo(titulo) {
     if (this.validarCadena('Titulo', titulo))
-      this.validarLongitudCadena('Titulo', titulo, 100) 
+      this.validarLongitudCadena('Titulo', titulo, 100)
+  }
+  validarDirector(director) {
+    if (this.validarCadena('Director', director))
+      this.validarLongitudCadena('Director', director, 50)
+  }
+  validarEstreno(estreno) {
+    if (this.validarNumero('Año de estreno', estreno))
+      if (!(/^([0-9]){4}$/.test(estreno)))
+        return console.error(`Año de estreno "${estreno}" no es valido, 
+        debe ser un numero de 4 digitos`)
+  }
+  validarPais(pais) {
+    this.validarArreglo('Pais', pais)
+  }
+  validarGeneros(generos) {
+    if (this.validarArreglo('Genero', generos)) {
+      for (const genero of generos) {
+        if (!Pelicula.listaGeneros.includes(genero)) {
+          console.error(`Genero incorrecto "${generos.join(', ')}`)
+          Pelicula.generosAceptados()
+        }
+      }
+    }
+  }
+
+  validarCalificacion(calificacion) {
+    if (this.validarNumero('Calificacion', calificacion))
+      return (calificacion < 0 || calificacion > 10)
+        ? console.error('La calificacion tiene que estar en un rango entre 0 y 10')
+        : this.calificacion = calificacion.toFixed(1)
+  }
+  fichaTecnica() {
+    console.info(`Ficha Tecnica:\nTitulo: "${this.titulo}"\nDirector:'${this.director}'\nAño: "${this.estreno}"\nPais:'${this.pais.join(',')}'
+  \nGeneros: "${this.generos.join(', ')}"\nCalificacion: "${this.calificacion}"\nIMDB id. "${this.id}"`)
   }
 }
 const peli = new Pelicula({
-  id: 'tt1234567'
+  id: 'tt1234567',
+  titulo: 'Hola Mundo',
+  director: 'Señor garcia',
+  estreno: 2020,
+  pais: ['Argentina', 'Uruguay', 'Paraguay'],
+  generos: ['Comedy'],
+  calificacion: 7.995
+
 })
+peli.fichaTecnica()
+
 
 
